2c7fe53f2e9ab4bb6983642e62b730c3b462a875
Sushil Khera <Sushilkhera@gmail.com>
2022-02-25 22:53:02 +0530

    Spring Boot Config Server

---
+++ src/test/java/com/sushil/config/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushil.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
